9adeaa1d8dea399242fd9e5da1ec2bc9cf86e18e
Evgeniy Marchal <evgeniymarchal@yandex.ru>
2024-07-28 12:12:26 +0200

    feat: added all files; seted toolbar menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
-import './App.css';
+import './styles/app.scss';
+import Toolbar from "./components/Toolbar";
+import SettingBar from "./components/SettingBar";
+import Canvas from "./components/Canvas";
+
+interface ICh {
+  data: number
+  // children?: JSX.Element|JSX.Element[];
+  children?: React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>
+}
 
 function App() {
+  const data = 6;
+
+  console.log(data);
+
+
+  const fn = (data: number) => {
+    return {data, id: 4, name: "Kont"}
+  }
+
+  const result = fn(data);
+
+  console.log(result)
+
   return (
     <div className="App">
-      Hello World
+      <Toolbar/>
+      <SettingBar/>
+      <Canvas/>
     </div>
   );
 }
